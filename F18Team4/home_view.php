f4995670518c4aabdbc499cf9e08c50b6327a9fe
Luke Nguyen <baanhnguyen92@gmail.com>
2018-12-04 13:11:41 -0500

    1. Add bootstrap 2. Change vertical menu to horizontal 3. Add cards to display feature products

--- F18Team4/home_view.php
+++ F18Team4/home_view.php
@@ -6,44 +6,38 @@
         guitars, basses, and drums. And we're constantly adding more to give
         you the best selection possible!
     </p>
-    <table>
-    <?php foreach ($products as $product) :
-        // Get product data
-        $list_price = $product['listPrice'];
-        $discount_percent = $product['discountPercent'];
-        $description = $product['description'];
-        
-        // Calculate unit price
-        $discount_amount = round($list_price * ($discount_percent / 100.0), 2);
-        $unit_price = $list_price - $discount_amount;
+    <div class="card-deck">
+        <?php
+        foreach ($products as $product) :
+            // Get product data
+            $list_price = $product['listPrice'];
+            $discount_percent = $product['discountPercent'];
+            $description = $product['description'];
 
-        // Get first paragraph of description
-        $description_with_tags = add_tags($description);
-        $i = strpos($description_with_tags, "</p>");
-        $first_paragraph = substr($description_with_tags, 3, $i-3);        
-    ?>
-        <tr>
-            <td class="product_image_column" >
-                <img src="images/<?php echo htmlspecialchars($product['productCode']); ?>_s.png"
-                     alt="&nbsp;">
-            </td>
-            <td>
-                <p>
-                    <a href="catalog?product_id=<?php echo
-                           $product['productID']; ?>">
-                        <?php echo htmlspecialchars($product['productName']); ?>
-                    </a>
-                </p>
-                <p>
-                    <b>Your price:</b>
-                    $<?php echo number_format($unit_price, 2); ?>
-                </p>
-                <p>
-                    <?php echo $first_paragraph; ?>
-                </p>
-            </td>
-        </tr>
-    <?php endforeach; ?>
-    </table>
+            // Calculate unit price
+            $discount_amount = round($list_price * ($discount_percent / 100.0), 2);
+            $unit_price = $list_price - $discount_amount;
+
+            // Get first paragraph of description
+            $description_with_tags = add_tags($description);
+            $i = strpos($description_with_tags, "</p>");
+            $first_paragraph = substr($description_with_tags, 3, $i - 3);
+            ?>
+
+            <div class="card">
+                <img class="card-img-top" width="286" height="180" 
+                     src="images/<?php echo htmlspecialchars($product['productCode']); ?>_s.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">
+                        <a href="catalog?product_id=<?php echo $product['productID']; ?>">
+                            <?php echo htmlspecialchars($product['productName']); ?>
+                        </a>
+                    </h5>
+                    <p class="card-text"><?php echo $first_paragraph; ?></p>
+                    <a href="#" class="btn btn-primary">See Detail</a>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </main>
 <?php include 'view/footer.php'; ?>
--- F18Team4/view/header.php
+++ F18Team4/view/header.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html>
 
-<!-- the head section -->
-<head>
-    <title>My Awesome Guitar Shop</title>
-    <link rel="stylesheet" type="text/css"
-          href="<?php echo $app_path ?>main.css">
-</head>
+    <!-- the head section -->
+    <head>
+        <title>My Awesome Guitar Shop</title>
+        <link rel="stylesheet" type="text/css" 
+              href="<?php echo $app_path ?>main.css">
+        <link rel="stylesheet" type="text/css" 
+              href="<?php echo $app_path ?>bootstrap/css/bootstrap.min.css">
+    </head>
 
-<body>
-<header>
-    <h1>My Guitar Shop</h1>
-</header>
+    <body>
+        <header>
+            <h1>My Guitar Shop</h1>
+        </header>
