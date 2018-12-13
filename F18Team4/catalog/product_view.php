b8e083907dd5f9e03d5f678004a07f8efbb4705d
Luke Nguyen <baanhnguyen92@gmail.com>
2018-12-13 16:05:34 -0500

    Redesign product page both for user and admin

--- F18Team4/catalog/product_view.php
+++ F18Team4/catalog/product_view.php
@@ -1,6 +1,6 @@
 <?php include '../view/header.php'; ?>
 <?php include '../view/sidebar.php'; ?>
-<main>
+<main class="nofloat">
     <!-- display product -->
     <?php include '../view/product.php'; ?>
 
