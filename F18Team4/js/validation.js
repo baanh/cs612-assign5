34012079771a67b7b4fea35b6eaa76414e92983d
Luke Nguyen <baanhnguyen92@gmail.com>
2018-12-13 14:27:31 -0500

    Add validation for product new/edit form

--- F18Team4/js/validation.js
+++ F18Team4/js/validation.js
@@ -1,6 +1,9 @@
 function validate(form) {
-    fail = validatePrice(form.price.value);
-    fail += validateDescription(form.description.value);
+    fail = validateDoubleNumber(form.price);
+    fail += validateDoubleNumber(form.discount_percent);
+    fail += validateRequired(form.code);
+    fail += validateRequired(form.name);
+    fail += validateRequired(form.description);
 
     if (fail === "") {
         return true;
@@ -10,19 +13,19 @@ function validate(form) {
     }
 }
 
-function validatePrice(field)
+function validateDoubleNumber(field)
 {
-    if (/[0-9]*\.[0-9]{2}/.test(field) === false) {
-        return "Must specify the number of dollars and cents for the price. Exp: 3 or 3.00";
+    if (/[0-9]*\.[0-9]{2}/.test(field.value) === false) {
+        return field.name.toUpperCase() + ": Must specify the number of dollars and cents. Exp: 3 or 3.00. \n";
     }
     
     return "";
 }
 
-function validateDescription(field)
+function validateRequired(field)
 {
-    if (field === "") {
-        return "Please add the description for the product";
+    if (field.value === "") {
+        return field.name.toUpperCase() + ": Required field. \n";
     }
     
     return "";
