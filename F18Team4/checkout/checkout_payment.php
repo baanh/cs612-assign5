8c2e4938f2051a4c5409cd813c27f6b28a9b6ba7
Schell <37990590+schellyuki@users.noreply.github.com>
2018-12-14 10:13:33 -0500

    Added estimated delivery date

--- F18Team4/checkout/checkout_payment.php
+++ F18Team4/checkout/checkout_payment.php
@@ -63,6 +63,34 @@
                             <small class="form-text text-muted">MM/YYYY.</small>
                         </div>
                     </div>
+                    
+                    <div class="col-sm-4">
+                        <?php
+                        function estimated_delivery_date($shipping_days = 3) {
+                            $today = time(); //timestamp for current time to be used for current date 
+                            $day_of_week = date("N", $today); // get the day of week, e.g. 1 = Monday through 7 = Sunday 
+                        
+                            if($day_of_week + $shipping_days < 6) {
+                                // delivery can be made within same business week
+                                $estimated_delivery_date = strtotime("+$shipping_days days");
+                            } else if(($day_of_week + $shipping_days) >= 6 && ($day_of_week + $shipping_days) < 15) {
+                                // delivery is possible next week hence add two days for weekend (Saturday and Sunday)
+                                $shipping_days += 2;
+                                $estimated_delivery_date = strtotime("+$shipping_days days");
+                                // check if new delivery date is falling in second weekend and adjust accordingly
+                                if(date("N", $estimated_delivery_date) == 6 || date("N", $estimated_delivery_date) == 7) {
+                                    $shipping_days += 2;
+                                    $estimated_delivery_date = strtotime("+$shipping_days days");
+                                }
+                            } else {
+                                // this function does not support shipping time > 7 days
+                                return "Not supported";
+                            }
+                            return date('jS M (D)', $estimated_delivery_date); // Format the date nicely.This format => 1st Jan (Wed) 
+                        }
+                        ?>
+                    </div>
+                    
                     <div class="col-sm-6">
                         <input type="submit" class="btn btn-success btn-block" value="Place Order">&nbsp;&nbsp;
                     </div>
